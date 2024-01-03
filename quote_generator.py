8a49ad9d0935aa36a4a50180ea96ff0d1af85cd1
Naguib <moma.naguib@gmail.com>
2024-01-03 15:00:00 +0100

    Ajout de la fonctionnalité principale : génération de citations aléatoires

--- quote_generator.py
+++ quote_generator.py
@@ -1,1 +1,10 @@
-print("Hello, world!")
+import json
+import random
+
+# Charger les citations depuis un fichier JSON
+with open("quotes.json", "r") as file:
+    quotes = json.load(file)
+
+# Afficher une citation aléatoire
+print("Voici une citation inspirante pour vous :")
+print(random.choice(quotes))
